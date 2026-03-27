66a60c6ad0b5d3f2a3605a782c40b9fbd9f5f783
LeHPhuc <plehoang641@gmail.com>
2026-03-27 20:26:44 +0700

    Doctor

--- cms/src/main/java/com/tqp/cms/config/SecurityConfig.java
+++ cms/src/main/java/com/tqp/cms/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import javax.crypto.spec.SecretKeySpec;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,6 +17,7 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtGra
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
     private static final String[] SWAGGER_ENDPOINT = {
             "/v3/api-docs/**",
